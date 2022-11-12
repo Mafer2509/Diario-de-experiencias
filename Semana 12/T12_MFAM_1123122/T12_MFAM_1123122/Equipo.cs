a199dd90881273aae71b931170719e0938928e30
Mafer2509 <114101707+Mafer2509@users.noreply.github.com>
2022-11-12 08:15:05 -0600

    Add files via upload

---
+++ Semana 12/T12_MFAM_1123122/T12_MFAM_1123122/Equipo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace T12_MFAM_1123122
+{
+    
+    class Equipo
+    {
+        internal string nombre;
+    }
+}
